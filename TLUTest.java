5a6043994b1c78d236aa557cc225426ce662270f
pyl14 <pyl14@software.nju.edu.cn>
2018-09-18 12:50:21 +0800

    add TLUtest for Problem2

--- TLUTest.java
+++ TLUTest.java
@@ -1,6 +1,8 @@
 package HelloTest;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class TLUTest {
 
@@ -8,9 +10,30 @@ public class TLUTest {
 
     int threshold=1;
 
+    ArrayList<ArrayList<Integer>> list=new ArrayList<>();
+
+
     private void getTuple(int[] arr,int changeIndex){
         if(changeIndex<0){
-            System.out.println(Arrays.toString(arr)+"--->"+ getRes(arr));
+            String res=Arrays.toString(arr);
+            res=res.replaceAll("\\[","");
+            res=res.replaceAll("\\]","");
+
+            int booleanRes=getRes(arr);
+
+            res=res+", "+ booleanRes;
+
+            if(booleanRes==1){
+                ArrayList<Integer> temp=new ArrayList<>();
+
+                for (int i:arr) {
+                    temp.add(i);
+                }
+
+                list.add(temp);
+            }
+
+            System.out.println(res.replaceAll(" ",""));
             return;
         }
 
@@ -38,11 +61,48 @@ public class TLUTest {
         }
     }
 
+    private String getLatex(){
+        StringBuilder stringBuilder=new StringBuilder("f(a,b,c,d,e)=");
+
+        for (int i = 0; i <list.size() ; i++) {
+            ArrayList<Integer> arr=list.get(i);
+
+            for (int j = 0; j <arr.size() ; j++) {
+                char c= (char) ('a'+j);
+
+                int res=arr.get(j);
+
+                if(res==0){
+                    //qu fan
+                    stringBuilder.append(" \\bar{"+c+"}");
+                }else{
+                    stringBuilder.append(" "+c);
+                }
+            }
+
+
+            if(i!=list.size()-1){
+                stringBuilder.append("+");
+            }
+        }
+
+        return stringBuilder.toString();
+    }
+
+
+
+
+
+
+
     public static void main(String[] args) {
         TLUTest test=new TLUTest();
 
         int[] arr=new int[test.myArr.length];
 
         test.getTuple(arr,arr.length-1);
+
+        System.out.println("=======Here is the boolean function:=======");
+        System.out.println(test.getLatex());
     }
 }
